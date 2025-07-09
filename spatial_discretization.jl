37fe587ed627737659d82bbff8e3f68d8c290fcc
Haakon Ludvig Langeland Ervik <45243236+haakon-e@users.noreply.github.com>
2025-07-09 11:35:12 -0700

    add boundary condition support

--- spatial_discretization.jl
+++ spatial_discretization.jl
@@ -1,12 +1,18 @@
-function horizontal_derivative!(dxdu, u, dx)
+function horizontal_derivative!(dxdu, u, dx; boundary_condition = :periodic)
     n = length(u)
     for i in 1:n
-        if i == 1
-            dxdu[i] = (u[i+1] - u[i]) / dx
-        elseif i == n
-            dxdu[i] = (u[i] - u[i-1]) / dx
+        if boundary_condition == :periodic
+            im1 = i == 1 ? n : i - 1
+            ip1 = i == n ? 1 : i + 1
+            dxdu[i] = (u[ip1] - u[im1]) / (2 * dx)
         else
-            dxdu[i] = (u[i+1] - u[i-1]) / (2 * dx)
+            if i == 1
+                dxdu[i] = (u[i+1] - u[i]) / dx
+            elseif i == n
+                dxdu[i] = (u[i] - u[i-1]) / dx
+            else
+                dxdu[i] = (u[i+1] - u[i-1]) / (2 * dx)
+            end
         end
     end
     nothing
